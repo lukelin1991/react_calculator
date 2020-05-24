0f3134ace5f13e701dc0e0cebcdf3d7af1d662e2
github username <github email address>
2020-05-24 12:19:13 -0400

    completed calculator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Display from './components/Display'
+import ButtonPanel from './components/ButtonPanel'
+import calculate from './components/logic/calculate'
+
 export default class App extends Component {
 
   state = {
@@ -9,10 +13,15 @@ export default class App extends Component {
     operation: null,
   }
 
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName))
+  }
+
   render(){
     return (
       <div className="App">
-
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick}/>
       </div>
     );
   }
